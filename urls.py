217a9c06c5eefded24126026b1419e07d56e8342
develbob <develbob@41ab8930-1a50-0410-8147-9d64cc925b63>
2008-07-22 13:59:35 +0000

    Still not working - but make sure I wont forget these files
    git-svn-id: https://metablog.googlecode.com/svn/trunk@6 41ab8930-1a50-0410-8147-9d64cc925b63

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+from django.views.generic.list_detail import object_list
+
+from models import *
+
+list_dict = { 'paginate_by' : 4,
+              'queryset' : Post.published()
+            }
+
+urlpatterns = patterns('',
+    (r'^$', 'object_list', list_dict.update({'page' : 1}))
+    (r'^page(?P<page>[0-9]+)/$', 'object_list', list_dict)
+)
+

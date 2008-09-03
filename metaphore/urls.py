1590dd0c84534f30e7b8fe429c886d9a0c25b547
Mathijs de Bruin <mathijs@visualspace.nl>
2008-09-03 11:06:41 +0200

    Generic view bug removed

--- metaphore/urls.py
+++ metaphore/urls.py
@@ -5,11 +5,9 @@ from django.views.generic.list_detail import object_list
 from models import *
 
 list_dict = { 'paginate_by' : 4,
-              'queryset' : Post.published()
-            }
+              'queryset' : Post.published.all() }
 
 urlpatterns = patterns('',
-    (r'^$', object_list, list_dict)
+    (r'^$', object_list, list_dict),
+    (r'^page(?P<page>[0-9]+)/$', object_list, list_dict)
 )
-# (r'^$', object_list, {'page' : 1})
-#  (r'^page(?P<page>[0-9]+)/$', object_list, list_dict)

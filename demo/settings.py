b7da9173601b489b44d84764fe6fdde2f87ae209
Mathijs de Bruin <mathijs@visualspace.nl>
2008-09-03 11:02:29 +0200

    Tweaked settings a little

--- demo/settings.py
+++ demo/settings.py
@@ -14,10 +14,7 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = project_root + '/database.sqlite'             # Or path to database file if using sqlite3.
-#DATABASE_ENGINE = 'mysql'
-#DATABASE_NAME = 'blogtest'
-#DATABASE_USER = 'root'             # Not used with sqlite3.
-
+DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -71,7 +68,7 @@ MIDDLEWARE_CLASSES = (
     #'sqllog.SQLLogMiddleware'
 )
 
-ROOT_URLCONF = 'blogtest.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".

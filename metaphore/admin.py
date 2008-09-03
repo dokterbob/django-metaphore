0a8d8f120b2e9d2430c10eb8aaf39192f2202621
Mathijs de Bruin <mathijs@visualspace.nl>
2008-09-03 11:38:08 +0200

    Cleanup of admin

--- metaphore/admin.py
+++ metaphore/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from models import *
 
 from django.utils.translation import ugettext as _
 
 from django import forms
+from django.forms.models import modelform_factory
+
+from models import *
 
 def get_default_sites():
     try:
@@ -27,31 +29,26 @@ class PostAdmin(admin.ModelAdmin):
         return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs) # Get the default field
             
     def get_fieldsets(self, request, obj=None):
+        post_form = modelform_factory(Post)
+        post_fields = post_form.base_fields.keys()
+        
         fieldsets_orig = super(PostAdmin, self).get_fieldsets(request, obj)
-        
-        post_fields = ['title','slug']
-        
-        # Only superusers can change authors, for now
-        if request.user.has_perm('change_author'):
-            post_fields.append('author')
-            
-        post_fields += ['description', 'site', 'publish', 'date_publish']
-            
-        fieldsets = [ (_('Post'), {'fields': post_fields }), ]
         
         fields_orig = fieldsets_orig[0][1]['fields']
         
-        for field in post_fields:
-            fields_orig.remove(field)
-            
-        # See above
-        if 'author' in fields_orig:
-            print 'Author removed'
+        if not request.user.has_perm('change_author'):
+            post_fields.remove('author')
             fields_orig.remove('author')
             
-        fields_orig.remove('links')
-        fieldsets.append( (_('Related posts'), {'fields': ['links',]}))
-            
+        for field in post_fields:
+            if field in fields_orig:
+                fields_orig.remove(field)
+        
+        post_fields.remove('links')
+        
+        fieldsets = [ (_('Post'), {'fields': post_fields }), 
+                      (_('Related posts'), {'fields': ['links',]}) ]
+        
         fieldsets.append( (_('Content'),  {'fields': fields_orig }) )
         
         return fieldsets

b9fe8e6bac9b8ad534c54cf4c9ebd7ddffd439c4
Mathijs de Bruin <drbob@dokterbob.net>
2010-02-24 16:52:20 +0100

    Actual oembed fetch support, hopefully.

--- metaphore/models.py
+++ metaphore/models.py
@@ -12,26 +12,43 @@ class OembedAbstractBase(PostAbstractBase):
     class Meta:
         abstract = True
     
+    # Title is already in there
+    
     url = models.URLField(verify_exists=True)
     
+    thumbnail_url = models.URLField(blank=True, verify_exists=True)
+    thumbnail_width = models.SmallIntegerField(blank=True, null=True)
+    thumbnail_height = models.SmallIntegerField(blank=True, null=True)
+
+    provider_name = models.CharField(blank=True, max_length=255)
+    provider_url = models.CharField(blank=True, max_length=255)
+    
+    author_name = models.CharField(blank=True, max_length=255)
+    author_url = models.URLField(blank=True, verify_exists=False)
+
     def save(self, *args, **kwargs):
+        from oembed import DefaultOEmbedConsumer        
+        
+        response = DefaultOEmbedConsumer.embed(self.url)
+        
+        for field in response:
+            if hasattr(self, field) and getattr(self, field) == None:
+                self.field = response[field]
         
         super(self, OembedAbstractBase).save(*args, **kwargs)
     
 class Link(OembedAbstractBase):
     pass
 
-class EmbeddedVideo(OembedAbstractBase):
+class EmbeddedRich(OembedAbstractBase):
     html = models.TextField()
     width = models.SmallIntegerField(blank=True, null=True)
     height = models.SmallIntegerField(blank=True, null=True)
-    
-    thumbnail_url = models.URLField(blank=True, verify_exists=True)
-    thumbnail_width = models.SmallIntegerField(blank=True, null=True)
-    thumbnail_height = models.SmallIntegerField(blank=True, null=True)
-    
-    author_name = models.CharField(blank=True, max_length=255)
-    author_url = models.URLField(blank=True, verify_exists=False)
-    
+
+class EmbeddedVideo(EmbeddedRich):        
     duration = models.SmallIntegerField(blank=True, null=True)
+    
+class EmbeddedPhoto(OembedAbstractBase):
+    width = models.SmallIntegerField(blank=True, null=True)
+    height = models.SmallIntegerField(blank=True, null=True)
     

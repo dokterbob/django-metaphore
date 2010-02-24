655a2b3bb61f744f3714b9ef1ab895fadfd90372
Mathijs de Bruin <drbob@dokterbob.net>
2010-02-24 15:44:03 +0100

    Initial (bogus) version of oembed code.

--- metaphore/models.py
+++ metaphore/models.py
@@ -8,5 +8,30 @@ class Article(PostAbstractBase):
 class Download(PostAbstractBase):
     download = models.FileField(upload_to='downloads')
 
-class Link(PostAbstractBase):
+class OembedAbstractBase(PostAbstractBase):
+    class Meta:
+        abstract = True
+    
     url = models.URLField(verify_exists=True)
+    
+    def save(self, *args, **kwargs):
+        
+        super(self, OembedAbstractBase).save(*args, **kwargs)
+    
+class Link(OembedAbstractBase):
+    pass
+
+class EmbeddedVideo(OembedAbstractBase):
+    html = models.TextField()
+    width = models.SmallIntegerField(blank=True, null=True)
+    height = models.SmallIntegerField(blank=True, null=True)
+    
+    thumbnail_url = models.URLField(blank=True, verify_exists=True)
+    thumbnail_width = models.SmallIntegerField(blank=True, null=True)
+    thumbnail_height = models.SmallIntegerField(blank=True, null=True)
+    
+    author_name = models.CharField(blank=True, max_length=255)
+    author_url = models.URLField(blank=True, verify_exists=False)
+    
+    duration = models.SmallIntegerField(blank=True, null=True)
+    

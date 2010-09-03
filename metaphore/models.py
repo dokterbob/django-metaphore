3e0f536d74a896e9bb1afb362b318f886d46cec9
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-03 14:03:20 +0200

    PEP8 cleanup

--- metaphore/models.py
+++ metaphore/models.py
@@ -4,46 +4,53 @@ from django.db import models
 
 from basemodels import Post, PostAbstractBase
 
+
 class Article(PostAbstractBase):
     text = models.TextField()
+
 
 class Download(PostAbstractBase):
     download = models.FileField(upload_to='downloads')
 
+
 class OembedAbstractBase(PostAbstractBase):
+
     class Meta:
         abstract = True
-    
+
     # Title is already in there
-    
+
     url = models.URLField(verify_exists=True)
-    
+
     thumbnail_url = models.URLField(blank=True, verify_exists=True)
     thumbnail_width = models.SmallIntegerField(blank=True, null=True)
     thumbnail_height = models.SmallIntegerField(blank=True, null=True)
 
     provider_name = models.CharField(blank=True, max_length=255)
     provider_url = models.CharField(blank=True, max_length=255)
-    
+
     author_name = models.CharField(blank=True, max_length=255)
     author_url = models.URLField(blank=True, verify_exists=False)
-    
+
+
 class Link(OembedAbstractBase):
     pass
+
 
 class EmbeddedRich(OembedAbstractBase):
     html = models.TextField(blank=True)
     width = models.SmallIntegerField(blank=True, null=True)
     height = models.SmallIntegerField(blank=True, null=True)
 
-class EmbeddedVideo(OembedAbstractBase):        
+
+class EmbeddedVideo(OembedAbstractBase):
     html = models.TextField(blank=True)
     width = models.SmallIntegerField(blank=True, null=True)
     height = models.SmallIntegerField(blank=True, null=True)
 
     duration = models.SmallIntegerField(blank=True, null=True)
-    
+
+
 class EmbeddedPhoto(OembedAbstractBase):
     width = models.SmallIntegerField(blank=True, null=True)
     height = models.SmallIntegerField(blank=True, null=True)
-    

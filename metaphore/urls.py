7a80c250225192b58f1875d19756c2871fc5db6b
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-20 15:47:11 +0200

    Fixed up feeds and added urls for it.

--- metaphore/urls.py
+++ metaphore/urls.py
@@ -1,16 +1,22 @@
 from django.conf.urls.defaults import *
 
+from metaphore.feeds import PostFeed
 
-urlpatterns = patterns('metaphore.views',
-   url(r'(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
+feed_urls = patterns('',
+    url(r'^rss/$', PostFeed(), name='metaphore-feed-rss'),
+)
+
+blog_urls = patterns('metaphore.views',
+   url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
          'metaphore_object_detail', name='metaphore-object-detail'),
-   url(r'(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/$',
+   url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/$',
          'metaphore_archive_day', name='metaphore-archive-day'),
-   url(r'(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+   url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
          'metaphore_archive_month', name='metaphore-archive-month'),
-   url(r'(?P<year>\d{4})/$',
+   url(r'^(?P<year>\d{4})/$',
          'metaphore_archive_year', name='metaphore-archive-year'),
-   url(r'$',
+   url(r'^$',
          'metaphore_archive_index', name='metaphore-archive-index'),
 )
 
+urlpatterns = feed_urls + blog_urls
